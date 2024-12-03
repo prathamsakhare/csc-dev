20d62d11b92c4ebd0580602d6a538b85cd5a6cee
PrathamSakhare <psakhre@themohh.com>
2024-12-04 00:04:59 +0530

    added onclick event for 'add {name}' button in dropdown list, created separate modal for add user form, implemented css and hide / show functionality using JS

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+// modals
 let modal = document.getElementById("modal");
 let overlay = document.getElementById("overlay");
 let addRecordButton = document.getElementById("record-add-button");
 let closeButton = document.getElementById("modal-close-btn");
 let saveButton = document.getElementById("modal-save-btn");
 
+let addUserModal = document.getElementById("addUserModal")
 
-// input fields for form of modal
+// input fields for form of add records modal
 let customerName = document.getElementById("name");
 let description = document.getElementById('desc')
 let category = document.getElementById('category')
 let amount = document.getElementById("amt")
 
+// input fields for form of add user modal
+let cutomerName = document.getElementById("username")
+let customerEmail = document.getElementById("useremail")
+let customerNumber = document.getElementById("usernumber")
+
+
 // elements for suggestion list for names
 let namelist = document.getElementById("namelist");
 let nameDropdown = document.getElementById("modal-name-dropdown");
 let nameSuggest = document.getElementById("name-suggest")
+
+// forms
+let recordInputForm = document.getElementById("recordInputForm");
+let userInputForm = document.getElementById("userInputForm")
 
 // * Approach 1 to add functionality - You can write function here and then you can add event listener to the html element you want to implement functionality on
 function openModal() {
@@ -22,6 +34,19 @@ function openModal() {
   modal.style.display = "block";
   overlay.style.display = "block";
 }
+
+recordInputForm.addEventListener("submit", function(event){
+  if (!event.submitter.matches('#modal-save-btn')) {
+		event.preventDefault();
+	}
+	
+})
+
+userInputForm.addEventListener("submit", function(event){
+  if (!event.submitter.matches('#modal-save-btn')) {
+		event.preventDefault();
+	}	
+})
 
 function closeModal() {
   modal.style.display = "none";
@@ -34,6 +59,18 @@ function closeModal() {
   amount.value = ''
   
 
+}
+
+function openAddUserModal(){
+  addUserModal.style.display = "block";
+}
+
+function closeAddUserModal(){
+  addUserModal.style.display = "none";
+
+  customerName.value = ""
+  customerEmail.value = ""
+  customerNumber.value = ""
 }
 
 // Optional feature - to hide modal whenever anywhere clicked on the screen
@@ -105,4 +142,5 @@ function setCustomerName(name){
   customerName.value = name;
   nameDropdown.style.display = 'none'
 }
+
 
